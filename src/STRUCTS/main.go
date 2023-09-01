4157db5f6d0c0d4be3d4c9d3449d7f4bb165ef14
MarioTiara <mario.tiarapratama@gmail.com>
2023-09-01 22:32:43 +0700

    Struct with pointer

--- src/STRUCTS/main.go
+++ src/STRUCTS/main.go
@@ -2,14 +2,46 @@ package main
 
 import "fmt"
 
+type contectInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contectInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(name string) {
+	(*p).firstName = name
 }
 
 func main() {
 	//alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
 
-	fmt.Println(alex.firstName)
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Pery",
+		contact: contectInfo{
+			email:   "jim@gmail.com",
+			zipCode: 9400,
+		},
+	}
+
+	//jim.print()
+	jimpointer := &jim
+	jimpointer.updateName("mario")
+
+	jim.print()
 }
